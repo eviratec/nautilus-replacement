92f098f15e70b83eaa50021e75d4993d6796ce1c
Callan Milne <callan.milne@eviratec.com.au>
2017-06-10 12:25:43 +1000

    Updates

--- main.js
+++ main.js
@@ -20,6 +20,7 @@
 
   var fs = require("fs");
   var path = require("path");
+  var child_process = require("child_process");
 
   var app = angular.module("nautilusReplacement", [
     "ngAnimate",
@@ -38,44 +39,172 @@
     $mdThemingProvider.definePalette('toolbarPalette', toolbarPalette);
 
     $mdThemingProvider.theme('toolbar')
-      .primaryPalette('toolbarPalette')
+      .primaryPalette('toolbarPalette');
+
+    // Get the current window
+    var win = nw.Window.get();
+    win.resizeTo(1000, 600);
 
   });
 
+  app.factory("FileSystemObject", FileSystemObjectFactory);
+
+  FileSystemObjectFactory.$inject = [];
+  function FileSystemObjectFactory () {
+
+    class FileSystemObject {
+      constructor (parentUri, info) {
+        this.parentUri = parentUri;
+        this.info = info;
+      }
+      get uri () {
+        return this.parentUri + "/" + this.basename;
+      }
+      get basename () {
+        return this.info.split(/\s+/g).slice(-1)[0];
+      }
+      get isHidden () {
+        let baseStartsWithDot = "." === this.info.split(/\s/g).slice(-1)[0][0];
+        return baseStartsWithDot;
+      }
+    }
+
+    return FileSystemObject;
+
+  }
+
+  app.factory("File", FileFactory);
+
+  FileFactory.$inject = ["FileSystemObject"];
+  function FileFactory (FileSystemObject) {
+
+    class File extends FileSystemObject {
+      constructor (parentUri, info) {
+        super(...arguments);
+      }
+    }
+
+    return File;
+
+  }
+
+  app.factory("Directory", DirectoryFactory);
+
+  DirectoryFactory.$inject = ["FileSystemObject"];
+  function DirectoryFactory (FileSystemObject) {
+
+    class Directory extends FileSystemObject {
+      constructor (parentUri, info) {
+        super(...arguments);
+      }
+    }
+
+    return Directory;
+
+  }
+
+  app.factory("NavigationList", NavigationListFactory);
+
+  NavigationListFactory.$inject = ["Directory", "File"];
+  function NavigationListFactory (  Directory,   File) {
+
+    class NavigationList {
+      constructor (parentUri, contents) {
+        this._contents = contents || [];
+        this.contents = this._contents.slice(0);
+        this.contents = this.contents.map(item => {
+          if ("d" === item.substr(0, 1)) {
+            return new Directory(parentUri, item);
+          }
+          return new File(parentUri, item);
+        });
+      }
+    }
+
+    return NavigationList;
+
+  }
+
   app.controller("WindowController", WindowController);
 
-  WindowController.$inject = ["$timeout"];
-  function WindowController (  $timeout) {
+  WindowController.$inject = ["$timeout", "NavigationList", "Directory"];
+  function WindowController (  $timeout,   NavigationList,   Directory) {
 
     var $w = this;
+
+    this.showHidden = false;
 
     this.info = {
       nodeVersion: process.version,
     };
 
+    this.locationHistory = [];
+
     this.location = {
-      str: process.env.HOME,
-      path: path.parse(process.env.HOME).dir,
-      dirname: path.parse(process.env.HOME).base,
+      str: "/",
+      path: "",
+      dirname: "",
       contents: [],
     };
 
+    this.favourites = {
+      contents: [
+        new Directory(process.env.HOME, "d Documents"),
+        new Directory(process.env.HOME, "d Downloads"),
+        new Directory(process.env.HOME, "d Music"),
+        new Directory(process.env.HOME, "d Pictures"),
+        new Directory(process.env.HOME, "d Projects"),
+        new Directory(process.env.HOME, "d Videos"),
+      ],
+    };
+
+    this.handleClick = function ($item, $event) {
+      let pathStr = path.resolve($item.uri);
+      if (pathStr !== $w.locationHistory[0]) {
+        $w.locationHistory.unshift(pathStr);
+      }
+      setLoc(pathStr);
+      reloadContents();
+    };
+
+    this.openMenu = function ($mdMenu, $event) {
+      originatorEv = $event;
+      $mdMenu.open($event);
+    };
+
+    setLoc(process.env.HOME);
     reloadContents();
 
     function reloadContents () {
       var contents;
-      $timeout(function () {
-        $w.location.contents.splice(0);
-        contents = fs.readdirSync(currentPath());
-        console.log(currentPath(),contents);
-        contents.forEach(item => {
-          $w.location.contents.push(item);
+      $w.location.contents.splice(0);
+      readdir(currentPath())
+        .then((r) => {
+          $timeout(function () {
+            r.contents.forEach(item => {
+              $w.location.contents.push(item);
+            });
+          });
+        });
+    }
+
+    function readdir (path) {
+      // return fs.readdirSync(path);
+      return new Promise((resolve, reject) => {
+        child_process.exec(`ls -alh`, {cwd: path}, function (err, res) {
+          resolve(new NavigationList(path, res.split(/\n/g).slice(2)));
         });
       });
     }
 
     function currentPath () {
       return $w.location.str;
+    }
+
+    function setLoc (uri) {
+      $w.location.str = uri;
+      $w.location.path = path.parse(uri).dir;
+      $w.location.dirname = path.parse(uri).base;
     }
 
   }
